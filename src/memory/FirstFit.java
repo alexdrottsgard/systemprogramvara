f271efc37e10de90cfca87161348c480f20e024b
drottsgaard <aalexjd@gmail.com>
2018-02-27 13:42:54 +0100

    allt

--- src/memory/FirstFit.java
+++ src/memory/FirstFit.java
@@ -1,8 +1,10 @@
 package memory;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
+import edu.princeton.cs.algs4.MinPQ;
 
 /**
  * This memory model allocates memory cells based on the first-fit method.
@@ -12,7 +14,6 @@ import java.util.LinkedList;
  */
 public class FirstFit extends Memory {
 	private LinkedList<Link> freeList = new LinkedList<Link>();
-	private int freeListSize;
 	private LinkedList<Link> usedList = new LinkedList<Link>();
 	private HashMap<Pointer, Integer> usedMap = new HashMap<Pointer, Integer>();
 
@@ -46,7 +47,6 @@ public class FirstFit extends Memory {
 	 */
 	public FirstFit(int size) {
 		super(size);
-		this.freeListSize = size;
 		freeList.add(new Link(size, new Pointer(0, this)));
 	}
 
@@ -59,28 +59,28 @@ public class FirstFit extends Memory {
 	 */
 	@Override
 	public Pointer alloc(int size) {
-		Pointer address = null;
-
+		Pointer nextAddress = null;
+		Pointer oldAddress = null;
 		for (Link linkFree : freeList) {
 			if (linkFree.size >= size) {
-				address = new Pointer(linkFree.pointer.pointsAt() + size, this);
+				oldAddress = new Pointer(linkFree.pointer.pointsAt(), this);
+				nextAddress = new Pointer(linkFree.pointer.pointsAt() + size, this);
 				// Om freeLinken blir 0, då tar vi bort den linken
 				if (linkFree.size == size) {
 					freeList.remove(linkFree);
 				} else {
 					linkFree.size -= size;
-					linkFree.pointer = address;
+					linkFree.pointer = nextAddress;
 				}
-				usedMap.put(address, size);
-				// usedList.add(new Link(size, address));
+				usedMap.put(oldAddress, size);
 				break;
 			}
 		}
-		if (address == null) {
+		if (nextAddress == null) {
 			System.out.println("Minnet för litet FeelsBadMan  (╯°□°）╯︵ ┻━┻)");
 		}
-		System.out.println("ADRESS: " + address.pointsAt());
-		return address;
+		System.out.println("ADDRESS: " + nextAddress.pointsAt());
+		return oldAddress;
 	}
 
 	/**
@@ -91,42 +91,42 @@ public class FirstFit extends Memory {
 	 */
 	@Override
 	public void release(Pointer p) {
-		int addressToRelease = p.pointsAt();
-		int sizeToRelease = usedMap.get(p);
-		int usedTotal = addressToRelease + sizeToRelease;
-		
-		int freeAddress = -1;
-		int index = -1;
-		Link link = null;
+		int addressToRelease = p.pointsAt(); //Adressen som ska tas bort
+		int sizeToRelease = usedMap.get(p); //Returnerar storleken på minnet som ska tas bort
+		int total = addressToRelease + sizeToRelease; //Adress + storlek på det som ska tas bort. Size + 1. (Denna är fel(?))
+
+		int freeAddress = -1;				// Ska lagra adressen på fria minnet.
+		int index = -1;						// Ska lagra index för aktuell position i for-loopen nedan
+
 		for (Link freeLink : freeList) {
-			index = freeList.indexOf(freeLink);
-			int freeSize = freeLink.size;
-			freeAddress = freeLink.pointer.pointsAt();
-			int freeTotal = freeSize + freeAddress;
+			index = freeList.indexOf(freeLink); 		// Index för freeLink
+			int freeSize = freeLink.size;				//storlek på freeLink i aktuell iterationen
+			freeAddress = freeLink.pointer.pointsAt();	// adress för freeLink i aktuell iteration
+			System.out.println("freeAddress: " + freeAddress);
+			int freeTotal = freeSize + freeAddress;		//total storlek för freeLink med adress Ex: adress = 70 & storlek = 30 alltså: tot = 100.
 			
-			if (freeTotal == addressToRelease) {
-				freeLink.size += sizeToRelease;
-				freeAddress = freeLink.pointer.pointsAt();
-				usedTotal = freeLink.size;
-				link = freeLink;
+			if (freeAddress < addressToRelease) {
+				if (freeTotal == addressToRelease) {
+					freeLink.size += sizeToRelease;
+					total = freeLink.size;
+					usedMap.remove(p);
+					break;
+				}
+			} else {
+				System.out.println("Skapar ny link som inte har några fria 'grannar'");
+				System.out.println("AddressToRelease: " + addressToRelease + " sizeToRelease: " + sizeToRelease);
+				Link newLink = new Link(sizeToRelease, new Pointer(addressToRelease, this));
+				freeList.add(index, newLink);
 				usedMap.remove(p);
 				break;
-			} else {
-				// skapa ny nod i freeList.
 			}
 		}
-		if (freeList.get(index + 1) == null) {
-			System.out.println("inget efter indexet i linklist");
-		} else if (freeList.get(index + 1).pointer.pointsAt() == usedTotal) {
-			System.out.println("freeList ska merga med index + 1");
-			link.size += freeList.get(index+1).size;
-		}
-//		for (Link freeLink : freeList) {
-//			if (freeAddress == usedTotal - 1) {
-//				
-//			}
-//		}
 
+		if (freeList.get(index + 1).pointer.pointsAt() == total) {
+			freeList.get(index).size += freeList.get(index + 1).size;
+			System.out.println("dubbelMergeSize = " + freeList.get(index).size);
+			freeList.remove(index + 1);
+		}
 	}
 
 	/**
@@ -137,8 +137,22 @@ public class FirstFit extends Memory {
 	 */
 	@Override
 	public void printLayout() {
+		LinkedList<Link> list = new LinkedList<Link>();
+		Link link = null;
+		
+		// sätter in linksen från HashMap till LinkedList
+		for (Pointer pointer : usedMap.keySet()) {
+			link = new Link(usedMap.get(pointer), pointer);
+			list.add(link);
 
-		// TODO Implement this!
+		}
+		list.sort(link);
+		
+		for (Link usedLink : list) {
+			System.out.println(usedLink.pointer.pointsAt());
+		}
+		
+//		 TODO Implement this!
 	}
 
 	/**
